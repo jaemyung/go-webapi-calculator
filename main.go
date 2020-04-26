1e4404584852a420f93cae7fd27d544fc510d11f
jae <jaekim.uw@gmail.com>
2020-04-26 15:13:14 -0400

    add power

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "strings"
     "strconv"
+    "math"
 )
 
 type Context struct {
@@ -111,7 +112,7 @@ func main() {
  
 
     r.HandleFunc("GET", "/", func(c *Context) {
-        fmt.Fprintln(c.ResponseWriter, "Welcome!")
+        fmt.Fprintln(c.ResponseWriter, "Waelcome!")
     })
  
     // 더하기 핸들러
@@ -121,6 +122,15 @@ func main() {
         
         fmt.Fprintf(c.ResponseWriter, "%v + %v = %v",
             n1, n2, n1 + n2)
+    })
+
+    // power will call math.Pow(number1,number2)
+    r.HandleFunc("GET", "/power/:number1/:number2", func(c *Context) {
+
+        n1, n2 := convParams2Int(c.Params["number1"], c.Params["number2"])
+        
+        fmt.Fprintf(c.ResponseWriter, "%v ^ %v = %v",
+            n1, n2, math.Pow(float64(n1),float64(n2)))
     })
 
     http.ListenAndServe(":8080", r)
